93ac58de1677184c8a6012f5e4d5db800f86faf4
Debi Prasad Sahoo <sahoodebiprasad874@gmail.com>
2024-11-04 22:48:18 +0530

    Update Azure_text_analytics.py

--- Azure_text_analytics.py
+++ Azure_text_analytics.py
@@ -1,5 +1,4 @@
-#there should be a environment file which will contain key and the endpoints 
-
+#there should be a environment variable which would contain the key and the endpoint of azure ai service and th rest thing will remain same
 
 
 
@@ -84,4 +83,42 @@ def GetLanguage(text):
 
 
 if _name_ == "_main_":
+    main()
+
+
+#this one is the clientsdk.py
+from dotenv import load_dotenv
+import os
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.textanalytics import TextAnalyticsClient
+
+def main():
+    global ai_endpoint
+    global ai_key
+    try:
+        # Get Configuration Settings
+        load_dotenv()
+        ai_endpoint = os.getenv('AI_SERVICE_ENDPOINT')
+        ai_key = os.getenv('AI_SERVICE_KEY')
+
+        # Get user input (until they enter "quit")
+        userText = ''
+        while userText.lower() != 'quit':
+            userText = input('\nEnter some text ("quit" to stop)\n')
+            if userText.lower() != 'quit':
+                language = GetLanguage(userText)
+                print('Language:', language)
+    except Exception as ex:
+        print(ex)
+
+def GetLanguage(text):
+    # Create client using endpoint and key
+    credential = AzureKeyCredential(ai_key)
+    client = TextAnalyticsClient(endpoint=ai_endpoint, credential=credential)
+
+    # Call the service to get the detected language
+    detectedLanguage = client.detect_language(documents=[text])[0]
+    return detectedLanguage.primary_language.name
+
+if __name__ == "__main__":
     main()
